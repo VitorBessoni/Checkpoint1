cfcbee9ecdf40ea1ed25f613dcbe903d7792142d
unknown <vitor.bessoni@estudante.ufjf.br>
2021-09-14 03:17:29 -0300

    salvando

--- parte01.js
+++ parte01.js
@@ -1,7 +1,30 @@
 
+document.querySelector("form").onsubmit= function(){
+    return false; }
+
 function criarCard(){
     title= document.getElementById("title").value;    
     let cardSection= document.getElementById("content");
+    cardSection.innerHTML+=`<div><h2>${title}</h2></div>`;
+    description= document.getElementById("description").value;
+    cardSection.innerHTML += `<div><p>${description}</p></div>`;
+    url = document.getElementById("url").value;
+    cardSection.innerHTML += `<div><img id="imagem" src="${url}"></div>`;
+}
+
+function criarCard2(){
+    title= document.getElementById("title").value;    
+    let cardSection= document.getElementById("content2");
+    cardSection.innerHTML+=`<div><h2>${title}</h2></div>`;
+    description= document.getElementById("description").value;
+    cardSection.innerHTML += `<div><p>${description}</p></div>`;
+    url = document.getElementById("url").value;
+    cardSection.innerHTML += `<div><img id="imagem" src="${url}"></div>`;
+}
+
+function criarCard3(){
+    title= document.getElementById("title").value;    
+    let cardSection= document.getElementById("content3");
     cardSection.innerHTML+=`<div><h2>${title}</h2></div>`;
     description= document.getElementById("description").value;
     cardSection.innerHTML += `<div><p>${description}</p></div>`;
